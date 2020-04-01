b5fbb1fa696ff8fbfb0a96c3b826bfe45a19fde7
beataworwa <mail.beata.worwa@gmail.com>
2020-04-01 22:23:04 +0200

    propertiesy w pomie.xml

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -24,7 +24,6 @@ public class FirstTest {
         waitForLoad(webDriver);
     }
 
-
     void waitForLoad(WebDriver driver) {
         new WebDriverWait(driver, 30).until((ExpectedCondition<Boolean>) wd ->
                 ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
